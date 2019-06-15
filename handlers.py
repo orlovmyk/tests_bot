1cf49779b2b16963c653654f1d5a6fe5882466a0
orlovw <orlownikita265@gmail.com>
2019-06-16 02:18:15 +0300

    Initial

--- handlers.py
+++ handlers.py
@@ -14,7 +14,7 @@ from telegram import (ReplyKeyboardMarkup,
                       InlineKeyboardMarkup)
 
 
-USERS = {}
+COUNTER = {}
 
 MAIN_MENU_MARKUP = ReplyKeyboardMarkup([['Начать тест', 'Начать тест'],
                                         ['Начать тест']],
@@ -61,6 +61,60 @@ def generate_inline(*step):
     return inline
 
 
+def add_kw(result, chat_id, message_id, **kwargs):
+    if kwargs:
+        new_kw = {
+            chat_id: {
+                message_id: {
+                    "result": result
+                }
+            }
+        }
+        COUNTER.update(new_kw)
+    else:
+        new_chat_kw = {
+            message_id: {
+                "result": result
+            }
+        }
+        COUNTER[chat_id].update(new_chat_kw)
+
+
+def count_result(message, result):
+    # 'message': {'message_id': 436, 'date': 1560638474, 'chat': {'id': 239062390,
+    chat_id = message.chat.id
+    message_id = message.id
+
+    if chat_id not in COUNTER.keys():
+        add_kw(result, chat_id, message_id)
+    else:
+        if message_id not in COUNTER[chat_id]:
+            add_kw(result, chat_id, message_id, to_message=True)
+        else:
+            COUNTER[chat_id][message_id].result += result
+
+
+def delete_query(message):
+    chat_id = message.chat.id
+    message_id = message.id
+    if chat_id in COUNTER.keys():
+        if message_id in COUNTER[chat_id]:
+            COUNTER[chat_id].pop(message_id)
+        else:
+            if not chat_id:
+                COUNTER.pop(chat_id)
+
+
+def get_result(message):
+    # 'message': {'message_id': 436, 'date': 1560638474, 'chat': {'id': 239062390,
+    chat_id = message.chat.id
+    message_id = message.id
+
+    if chat_id in COUNTER.keys():
+        if message_id in COUNTER[chat_id]:
+            return COUNTER[chat_id][message_id]["result"]
+
+
 # MESSAGE HANDLER
 def start(bot, update):
     update.message.reply_text(INITIAL_MESSAGE,
@@ -88,17 +142,20 @@ def callback_handler(bot, update):
     step = int(step) + 1
 
     if step > len(TEXT):
+        count_result(query.message, result)
         bot.edit_message_text(chat_id=query.message.chat_id,
                               message_id=query.message.message_id,
                               text=TEXT[step],
                               reply_markup=generate_inline())
-        bot.answer_callback_query(callback_query_id=query.id, text="На этом всё")
+        bot.answer_callback_query(callback_query_id=query.id, text="На этом всё\nВаш результат: " + str(get_result(query.message)))
+        delete_query(query.message)
 
     else:
+        count_result(query.message, result)
         bot.edit_message_text(chat_id=query.message.chat_id,
                               message_id=query.message.message_id,
                               text=TEXT[step],
-                              reply_markup=generate_inline())
+                              reply_markup=generate_inline(step))
         bot.answer_callback_query(callback_query_id=query.id, text="Ви выбрали вариант №" + str(result))
 
 
