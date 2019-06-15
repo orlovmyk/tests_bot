fa176a3156051c0402e15dba959e3549b17cd250
orlovw <orlownikita265@gmail.com>
2019-06-15 19:30:18 +0300

    Initial

--- handlers.py
+++ handlers.py
@@ -19,9 +19,34 @@ USERS = {}
 MAIN_MENU_MARKUP = ReplyKeyboardMarkup([['Начать тест', 'Начать тест'],
                                         ['Начать тест']],
                                        resize_keyboard=True)
-MARK, CONTACT = range(2)
 
 
+TEXT = ["Вопрос №1 Вариатны такие:\n"
+        "1. выфвыф\n"
+        "2. выфвы\n" 
+        "3. выф выфвыф\n",
+
+        "Вопрос №2 Вариатны такие:\n"
+        "1. выфвыф\n"
+        "2. выфвы\n"
+        "3. выф выфвыф\n",
+
+        "Вопрос №3 такие:\n"
+        "1. выфвыф\n"
+        "2. выфвы\n"
+        "3. выф выфвыф\n",
+        ]
+
+
+# USEFUL FUNCTIONS
+def generate_inline(step):
+    inline = [[InlineKeyboardButton(text="1", callback_data=str(step)+".1")],
+              [InlineKeyboardButton(text="2", callback_data=str(step)+".2")],
+              [InlineKeyboardButton(text="3", callback_data=str(step)+".3")]]
+    return inline
+
+
+# MESSAGE HANDLER
 def start(bot, update):
     update.message.reply_text('Привет!\n'
                               'Я чат-бот, созданый для ...\n\n',
@@ -29,14 +54,9 @@ def start(bot, update):
 
 
 def test_start(bot, update):
-    message = "Первый вопрос\n" \
-              "1. выфвы\n" \
-              "2. выфвы\n" \
-              "3. выфвыфвыф\n"
-    markup = InlineKeyboardMarkup([[InlineKeyboardButton(text="1", callback_data="1.1")],
-                                   [InlineKeyboardButton(text="2", callback_data="1.2")],
-                                   [InlineKeyboardButton(text="3", callback_data="1.3")],
-                                   ])
+    message = TEXT[1]
+    markup = InlineKeyboardMarkup(generate_inline(0))
+
     update.message.reply_text(message,
                               reply_markup=markup)
 
@@ -48,7 +68,22 @@ def main_menu(bot, update):
 
 # CALLBACK HANDLER
 def callback_handler(bot, update):
-    print(123)
+    query = update.callback_query
+    print(query)
+
+    step, result = query.strip(".")
+    step += 1
+
+    if step > len(TEXT):
+        bot.edit_message_text(chat_id=query.message.chat_id,
+                              message_id=query.message.message_id,
+                              text="Конец",
+                              reply_markup=[[]])
+
+    bot.edit_message_text(chat_id=query.message.chat_id,
+                          message_id=query.message.message_id,
+                          text=TEXT[step],
+                          reply_markup=generate_inline(step))
 
 
 bot_handlers = [CommandHandler('start', start),
