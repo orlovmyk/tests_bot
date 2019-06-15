d03422e49c4bb4debe99fdf473a2c0025a6ef809
orlovw <orlownikita265@gmail.com>
2019-06-15 19:07:03 +0300

    Initial

--- app.py
+++ app.py
@@ -13,13 +13,14 @@ from telegram.ext import Updater
 port = int(environ.get('PORT', 5000))
 token = environ.get('BOT_TOKEN')
 domain = environ.get('DOMAIN', '')
+url = environ.get('URL', '/')
 
 bot_updater = Updater(token)
 
 
 def start_listen():
     """Begin listening"""
-    bot_updater.start_webhook(url_path=domain,
+    bot_updater.start_webhook(url_path="/",
                               port=port,
                               listen='0.0.0.0')
     bot_updater.bot.set_webhook("https://"+domain+"/")
--- handlers.py
+++ handlers.py
@@ -1,9 +1,17 @@
 """
 Handlers for all commands
 """
-from telegram.ext import (CommandHandler, MessageHandler, Filters,
-                          RegexHandler, ConversationHandler)
-from telegram import ReplyKeyboardMarkup, KeyboardButton
+from telegram.ext import (CommandHandler,
+                          MessageHandler,
+                          Filters,
+                          RegexHandler,
+                          ConversationHandler,
+                          CallbackQueryHandler,)
+
+from telegram import (ReplyKeyboardMarkup,
+                      KeyboardButton,
+                      InlineKeyboardButton,
+                      InlineKeyboardMarkup)
 
 
 USERS = {}
@@ -20,6 +28,19 @@ def start(bot, update):
                               reply_markup=MAIN_MENU_MARKUP)
 
 
+def test_start(bot, update):
+    message = "Первый вопрос" \
+              "1. выфвы" \
+              "2. выфвы" \
+              "3. выфвыфвыф"
+    markup = InlineKeyboardMarkup([[InlineKeyboardButton(text="1", callback_data="1.1")],
+                                   [InlineKeyboardButton(text="2", callback_data="1.2")],
+                                   [InlineKeyboardButton(text="3", callback_data="1.3")],
+                                   ])
+    update.message.reply_text(message,
+                              reply_markup=markup)
+
+
 def main_menu(bot, update):
     update.message.reply_text('Главное меню',
                               reply_markup=MAIN_MENU_MARKUP)
@@ -27,6 +48,7 @@ def main_menu(bot, update):
 
 bot_handlers = [CommandHandler('start', start),
                 RegexHandler('Главное меню', main_menu),
+                RegexHandler('Начать тест', test_start),
 
 
 
